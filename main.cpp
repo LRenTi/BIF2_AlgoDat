ca3caa6434e71f475d5674f043a926e6595427ff
shammerschmidt <if23b042@technikum-wien.at>
2024-02-27 15:49:36 +0100

    Class creation

--- main.cpp
+++ main.cpp
@@ -11,10 +11,22 @@ enum options{
     QUIT
 };
 
+class stock{
+    public:
+        std::string date;
+        std::string name;
+        std::string abbr;
+        std::string wkn;
+        float open;
+        float high;
+        float low;
+        float close;
+        float adj_close;
+        int volume;
+};
+
 int main(){
 
     int input;
-
-
 
 }
