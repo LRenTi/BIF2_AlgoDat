901bfe6e452f1caf3026ba1352af1a7a8fa53daf
Samuel <122015996+shammerschmidt1999@users.noreply.github.com>
2024-04-17 13:55:03 +0200

    Delete main.exe

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ def main():
     print("min:", min_key, "max:", max_key, "avg:", avg_key)
 
     # Wenn nur ein Eintrag im subtree ist, beginne mit Einfacher Suche
-    # Wenn nur ein Eintrag im subtree ist, beginne mit Einfacher Suche
     if len(subTreeKeys) == 1:
         t.simpleSearch(mainTreeRoot, subTreeRoot, nodes)
 
