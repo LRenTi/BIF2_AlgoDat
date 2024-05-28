6d2c4034026d7bf0be932c35d4312a4837f12d77
Samuel Hammerschmidt <samuel.hammerschmidt@gmx.at>
2024-05-28 08:49:59 +0200

    Dijkstra implementation, shortest path output

--- main.py
+++ main.py
@@ -30,7 +30,45 @@ class Graph:
         connection = Connection(line, self.stations[from_station], cost)
         self.stations[to_station].add_connection(connection)
 
-    # TODO: Dijkstra implementation (Search for the shortest path)
+
+    # Dijkstra implementation (Search for the shortest path)
+    def dijkstra(self, start, end):
+        # Initialize the shortest distances and previous stations
+        shortest_distances = {station: float('infinity') for station in self.stations.values()} # Initialize with infinity
+        previous_stations = {station: None for station in self.stations.values()} # Initialize with None
+        shortest_distances[self.stations[start]] = 0 # Start station has distance 0
+        unvisited_stations = set(self.stations.values()) # Set of all stations
+
+        # Search for the shortest path
+        while unvisited_stations:
+            current_station = min(unvisited_stations, key=lambda station: shortest_distances[station]) # Get the station with the shortest distance
+            unvisited_stations.remove(current_station) # Remove the current station from the set of unvisited stations
+
+            # Check if the destination station is reached
+            if current_station == self.stations[end]: 
+                # Reconstruct the path
+                path = []
+                line = None
+                while previous_stations[current_station] is not None:
+                    for connection in previous_stations[current_station].connections:
+                        if connection.to_station == current_station:
+                            line = connection.line
+                            break
+                    path.append((line, current_station.name))  # Line of the connection
+                    current_station = previous_stations[current_station]  # Move to the previous station
+                path.append((None, start))  # Start station has no line
+                path.reverse()  # Reverse the path
+                return path, shortest_distances[self.stations[end]]  # Return the path and the total cost
+
+            # Update the shortest distances and previous stations
+            for connection in current_station.connections:
+                distance = shortest_distances[current_station] + connection.cost
+                if distance < shortest_distances[connection.to_station]:
+                    shortest_distances[connection.to_station] = distance
+                    previous_stations[connection.to_station] = current_station
+
+        # If no path was found
+        return None, float('infinity')
 
 
 # Nur zur Überprüfung - nicht Teil der Lösung
@@ -62,6 +100,21 @@ def parse_graph(filename):
     
     return graph
 
+def print_shortest_path(path, distance, start, end):
+    print("The shortest path from {} to {} is: \n".format(start, end), end="")
+    prev_line = None
+    for i, (line, station) in enumerate(path):
+        if line is not None:
+            if prev_line is not None and line != prev_line:
+                print("[switch from {} to {}]".format(prev_line, line), end=" ")
+            print("--> \n{}: {}".format(line, station), end=" ")
+            if i == len(path) - 1:  # If this is the final station
+                print("\n<<< You arrived at your destination {} >>>".format(station))
+        else:
+            print(station, end=" ")  # Start station has no line
+        prev_line = line
+    print("\nThe total cost/duration of this path is: {}".format(distance))
+
 def main():
     # Argument parsing
     parser = argparse.ArgumentParser()
@@ -77,8 +130,6 @@ def main():
     
     graph = parse_graph(filename)
     
-    # TODO: Search for the shortest path
-    
     if args.pretty_print and graph is not None:
         if start is not None:
             for station_name, station in graph.stations.items():
@@ -93,7 +144,15 @@ def main():
             print_graph(graph)
             print("-----------------------------")
     
-    # TODO: Output of the shortest path (Which stations to take, which lines to use, where to transfer, total cost)
+    # Search for the shortest path
+    path, distance = graph.dijkstra(start, end)
+
+    # Output of the shortest path (Which stations to take, which lines to use, where to transfer, total cost)
+    print_shortest_path(path, distance, start, end)
+    # TODO: Looks good, only one weird line switch from "Kagraner Platz" -> Migerkastrasse
+    # Should be Quellenstrasse / Favoritenstrasse --> Quellenplatz (NO SWITCH) --> ...
+    # Maybe not noticeable :P
+
 
 if __name__ == "__main__":
     main()
